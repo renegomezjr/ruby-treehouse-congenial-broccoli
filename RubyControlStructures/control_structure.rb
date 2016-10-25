38811eb4be2d4f66b8d16a31838b05089aec11e2
Rene Gomez <renegomezjr@gmail.com>
2016-10-25 19:00:17 -0400

    Control structures

---
+++ RubyControlStructures/control_structure.rb
@@ -0,0 +1,11 @@
+car1_speed = 500
+car2_speed = 300
+
+if car1_speed > car2_speed
+  puts "Car 1 is faster than car 2 speed!"
+end
+
+car2_speed += 300
+if car2_speed > car1_speed
+  puts "Car 2 is faster than car 1 speed!"
+end
